7a308f74f7f6b64a00d53c5b7c75842784c545cb
ZhykharievAnatolii <nekijt87@gmail.com>
2022-09-04 10:55:12 +0300

    new upd

--- src/index.js
+++ src/index.js
@@ -65,5 +65,6 @@ export const getFlooroCeil = (number) => {
  * @returns {number}
  */
 export const randomizer = (from, to) => {
-    return Math.floor(from + Math.random() * (from - to))
+    Math.floor(from + Math.random() * (from - to))
+    return Math.random(number)
 };
